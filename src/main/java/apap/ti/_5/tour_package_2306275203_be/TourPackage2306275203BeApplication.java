e81b5f30a501922135ca076f4906c0836e27084b
Malika Atha Indurasmi <malika.atha31@ui.ac.id>
2025-11-03 02:11:06 +0700

    initialize backend project

---
+++ src/main/java/apap/ti/_5/tour_package_2306275203_be/TourPackage2306275203BeApplication.java
@@ -0,0 +1,13 @@
+package apap.ti._5.tour_package_2306275203_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourPackage2306275203BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourPackage2306275203BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/_5/tour_package_2306275203_be/TourPackage2306275203BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.tour_package_2306275203_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourPackage2306275203BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
