e237c6404a201d873ad0c596d2b60e04b15afe1b
Malika Atha Indurasmi <malika.atha31@ui.ac.id>
2025-11-22 15:58:38 +0700

    issue controller

--- src/main/java/apap/ti/_5/tour_package_2306275203_be/TourPackage2306275203BeApplication.java
+++ src/main/java/apap/ti/_5/tour_package_2306275203_be/TourPackage2306275203BeApplication.java
@@ -3,7 +3,7 @@ package apap.ti._5.tour_package_2306275203_be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "apap.ti._5.tour_package_2306275203_be")
 public class TourPackage2306275203BeApplication {
 
 	public static void main(String[] args) {
