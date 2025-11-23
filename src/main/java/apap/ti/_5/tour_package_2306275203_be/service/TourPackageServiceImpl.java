58c8dd1296a07cbf90a9b2332cdcfe46f6b61c78
Malika Atha Indurasmi <malika.atha31@ui.ac.id>
2025-11-24 01:41:31 +0700

    T1-2 startDate asc

--- src/main/java/apap/ti/_5/tour_package_2306275203_be/service/TourPackageServiceImpl.java
+++ src/main/java/apap/ti/_5/tour_package_2306275203_be/service/TourPackageServiceImpl.java
@@ -14,6 +14,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -71,6 +72,7 @@ public class TourPackageServiceImpl implements TourPackageService {
     @Override
     public List<TourPackageResponseDTO> getAllTourPackage() {
         return tourPackageDb.findAll().stream()
+                .sorted(Comparator.comparing(TourPackage::getStartDate)) 
                 .map(this::convertToResponseDTO)
                 .collect(Collectors.toList());
     }
